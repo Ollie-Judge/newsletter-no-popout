6a02a8d393e3224ef656328cbc27ca83543d32d3
Ollie-Judge <ollie.judge@hotmail.com>
2022-12-19 15:45:35 +0000

    copied all of this from my previous repo

---
+++ script.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const express = app();
+
+app.listen(app, function () {
+  console.log("server is running on port 3000");
+});
